9b3052c59b6a24cde59b2d70ddc2fb6beb862b28
Adnan Aziz <adnan.832@gmail.com>
2021-05-24 20:39:38 +0200

    requested changes are implemented

--- src/backend/api/meals.js
+++ src/backend/api/meals.js
@@ -2,44 +2,49 @@ const express = require("express");
 const router = express.Router();
 const knex = require("../database");
 
-
 // GET api/meals/ query parameters
 
 router.get("/", async (request, response) => {
   try {
     if (request.query.maxPrice) {
       const maxPrice = parseInt(request.query.maxPrice);
-      const mealsWithMaxPrice = await knex("meals").where("price", "<", maxPrice);
+      const mealsWithMaxPrice = await knex("meals").where(
+        "price",
+        "<",
+        maxPrice
+      );
       response.json(mealsWithMaxPrice);
-    }
-    else if (request.query.tittle) {
-      const givenTittle = request.query.tittle;
-      const mealsWithTittle = await knex('meals')
-        .where("tittle", 'like', '%' + givenTittle + '%');
-      response.json(mealsWithTittle);
-
-    }
-    else if (request.query.limit) {
+    } else if (request.query.title) {
+      const givenTitle = request.query.title;
+      const mealsWithTittle = await knex("meals").where(
+        "tittle",
+        "like",
+        "%" + givenTitle + "%"
+      );
+      response.json(mealsWithTitle);
+    } else if (request.query.limit) {
       const givenLimit = parseInt(request.query.limit);
-      const mealsWithLimit = await knex('meals')
-        .limit(givenLimit);
+      const mealsWithLimit = await knex("meals").limit(givenLimit);
 
       response.json(mealsWithLimit);
-
-    }
-    else if (request.query.availableReservations) {
-      const mealsWithReservations = await knex('meals')
-        .where("max_reservations", ">", 0);
+    } else if (request.query.availableReservations) {
+      const mealsWithReservations = await knex("meals").where(
+        "max_reservations",
+        ">",
+        0
+      );
 
       response.json(mealsWithReservations);
-    }
-    else if (request.query.createdAfter) {
+    } else if (request.query.createdAfter) {
       const givenDate = new Date(request.query.createdAfter);
-      const mealsCreatedAfterDate = await knex('meals').where('created_date', '>', givenDate);
+      const mealsCreatedAfterDate = await knex("meals").where(
+        "created_date",
+        ">",
+        givenDate
+      );
 
       response.json(mealsCreatedAfterDate);
-    }
-    else {
+    } else {
       const titles = await knex("meals").select("tittle");
       const meals = await knex("meals");
       response.json(meals);
@@ -49,16 +54,13 @@ router.get("/", async (request, response) => {
   }
 });
 
-
 // get, post, put and delete
 
 router.post("/", async (request, response) => {
   console.log(request.body);
   try {
-    await knex("meals")
-      .insert(request.body)
+    await knex("meals").insert(request.body);
     response.json("meal has been added");
-
   } catch (error) {
     response.status(500).json("Internal server error");
   }
@@ -66,8 +68,9 @@ router.post("/", async (request, response) => {
 
 router.get("/:id", async (request, response) => {
   try {
-    const mealByid = await knex("meals")
-      .where({ id: parseInt(request.params.id) });
+    const mealByid = await knex("meals").where({
+      id: parseInt(request.params.id),
+    });
     response.json(mealByid);
   } catch (error) {
     response.status(500).json("Internal server error");
@@ -91,7 +94,6 @@ router.delete("/:id", async (request, response) => {
       .where({ id: parseInt(request.params.id) })
       .del();
     response.json(deletedMeal);
-
   } catch (error) {
     throw error;
   }
--- src/backend/api/reservations.js
+++ src/backend/api/reservations.js
@@ -3,56 +3,54 @@ const router = express.Router();
 const knex = require("../database");
 
 router.get("/", async (request, response) => {
-    try {
-        const reservations = await knex("reservation");
-        response.json(reservations);
-    } catch (error) {
-        throw error;
-    }
+  try {
+    const reservations = await knex("reservation");
+    response.json(reservations);
+  } catch (error) {
+    throw error;
+  }
 });
 
 router.post("/", async (request, response) => {
-    try {
-        await knex("reservations").insert(request.body);
-        response.json("Reservation is added");
-    } catch (error) {
-        throw error
-    }
-
+  try {
+    await knex("reservations").insert(request.body);
+    response.json("Reservation is added");
+  } catch (error) {
+    throw error;
+  }
 });
 
 router.get("/:id", async (request, response) => {
-    try {
-        const reservationsById = await knex('reservation')
-            .where({ id: parseInt(request.params.id) });
-        response.json(reservationsById);
-    } catch (error) {
-        throw error;
-    }
+  try {
+    const reservationsById = await knex("reservation").where({
+      id: parseInt(request.params.id),
+    });
+    response.json(reservationsById);
+  } catch (error) {
+    throw error;
+  }
 });
 
-
-router.put('/:id', async (request, response) => {
-    try {
-        const updatedReservations = await knex('reservation')
-            .where({ id: parseInt(request.params.id) })
-            .update(request.body);
-        response.json('Reservation updated');
-    } catch (error) {
-        throw error;
-    }
+router.put("/:id", async (request, response) => {
+  try {
+    const updatedReservations = await knex("reservation")
+      .where({ id: parseInt(request.params.id) })
+      .update(request.body);
+    response.json("Reservation updated");
+  } catch (error) {
+    throw error;
+  }
 });
 
-
-router.delete('/:id', async (request, response) => {
-    try {
-        const deletedReservation = await knex('reservation')
-            .where({ id: parseInt(request.params.id) })
-            .del();
-    } catch (error) {
-        throw error;
-    }
-    response.json('Reservation deleted');
+router.delete("/:id", async (request, response) => {
+  try {
+    const deletedReservation = await knex("reservation")
+      .where({ id: parseInt(request.params.id) })
+      .del();
+  } catch (error) {
+    throw error;
+  }
+  response.json("Reservation deleted");
 });
 
 module.exports = router;
